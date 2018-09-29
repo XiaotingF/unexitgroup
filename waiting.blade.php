af9ec5d3b97a6fcca5ab7bb55906f06ce108a9a7
s3584000 <41462755+s3584000@users.noreply.github.com>
2018-09-29 13:55:41 +1000

    update

--- waiting.blade.php
+++ waiting.blade.php
@@ -13,6 +13,56 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
+                    <div class="card-deck">
+                     <?php if(is_object($waiting1)) : ?>
+                        <div class="col-lg-4">
+                      <div class="card text-center" >
+                                <img class="card-img-top" src="/uploads/avatars/{{ $waiting1->avatar }}" style="width:200px; height:200px; ">
+                                <div class="card-body">
+                                  <h5 class="card-title">{{$waiting1->name}}</h5>
+                                  <p class="card-text">Name: {{$waiting1->name}}<br>
+                                    Gender: {{$waiting1->gender}}
+                                  </p>
+                                <form action="" method="post">
+                                <button class="btn btn-primary" type = "submit" >Accept</button>
+                                </form>
+                                <br>
+                                <form action="" method="post">
+                                <button class="btn btn-primary" type = "submit" >Reject</button>
+                                </form>
+                                <br>
+                                </div>
+                              
+                                <div class="card-footer">
+                                  <small class="text-muted"></small>
+                                </div>
+                      </div>
+                  </div>
+                  <?php endif; ?>
+
+
+                  <?php if(is_object($waiting2)) : ?>
+                      <div class="col-lg-4">
+                      <div class="card text-center" >
+                                <img class="card-img-top" src="/uploads/avatars/{{ $waiting2->avatar }}" style="width:200px; height:200px; ">
+                                <div class="card-body">
+                                  <h5 class="card-title">{{$waiting2->name}}</h5>
+                                  <p class="card-text">Name: {{$waiting2->name}}<br>
+                                    Gender: {{$waiting2->gender}}
+                                  </p>
+                                </div>
+                                <button class="btn btn-primary" type = "submit" >Accept</button>
+                                <div class="card-footer">
+                                  <small class="text-muted"></small>
+                                </div>
+                      </div>
+                  </div>
+                  <?php endif; ?>
+                 
+           
+                    </div>
+
                 </div>
 <br>
 
