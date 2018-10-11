ae26380b5fd1f97977c632b1b6d0bde2404dbbc4
XiaotingF <s3626819@student.rmit.edu.au>
2018-10-11 13:56:26 +1100

    add more text

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'slug', 'gender','admin', 'status'
+        'name', 'email', 'password', 'gender','admin', 'status'
     ];
 
     /**
